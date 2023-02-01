f0766b7397b9fdc8eda1790075b948286f32eb1c
Michael Boke <michael@mbict.nl>
2023-02-01 12:32:25 +0100

    adds StatusCoder interface implementation to get the http status code for an error + 2 convenience constructors

--- error.go
+++ error.go
@@ -1,6 +1,9 @@
 package domain
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Error interface {
 	error
@@ -9,6 +12,7 @@ type Error interface {
 }
 
 type domainError struct {
+	code      int
 	message   string
 	domain    string
 	aggregate string
@@ -21,6 +25,11 @@ func (e *domainError) MarshalJSON() ([]byte, error) {
 
 func (e *domainError) Error() string {
 	return e.message
+}
+
+// StatusCode should return the http status code used to determine the error
+func (e *domainError) StatusCode() int {
+	return e.code
 }
 
 func (e *domainError) Domain() string {
@@ -43,6 +52,7 @@ func (e *domainError) With(key string, value interface{}) Error {
 	metadata[key] = value
 
 	return &domainError{
+		code:      e.code,
 		message:   e.message,
 		domain:    e.domain,
 		aggregate: e.aggregate,
@@ -51,8 +61,17 @@ func (e *domainError) With(key string, value interface{}) Error {
 }
 
 func NewError(message string, domain string) Error {
+	return NewErrorWithCode(message, domain, http.StatusBadRequest)
+}
+
+func NewErrorWithCode(message string, domain string, code int) Error {
 	return &domainError{
+		code:    code,
 		message: message,
 		domain:  domain,
 	}
+}
+
+func NewNotFoundError(message string, domain string) Error {
+	return NewErrorWithCode(message, domain, http.StatusNotFound)
 }
